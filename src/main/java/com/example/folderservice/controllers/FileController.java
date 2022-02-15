07648e752aa827af02e90a0c8a7da6d6d91e0a6d
Marijke Drenth <drenth.marijke@googlemail.com>
2022-02-15 18:55:48 +0200

    Fix spacing

--- src/main/java/com/example/folderservice/controllers/FileController.java
+++ src/main/java/com/example/folderservice/controllers/FileController.java
@@ -1,9 +1,6 @@
 package com.example.folderservice.controllers;
-
 import com.example.folderservice.models.File;
-import com.example.folderservice.models.User;
 import com.example.folderservice.repositories.FileRepository;
-import com.example.folderservice.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
